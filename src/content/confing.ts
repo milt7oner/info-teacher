9f8c6f1629ad2576320bf8e5419a90e5cb5b5887
mil7oner <duvans427@gmail.com>
2025-08-15 21:14:11 -0500

    Agregamos calendario

---
+++ src/content/confing.ts
@@ -0,0 +1,42 @@
+import { defineCollection, z } from "astro:content";
+
+const events = defineCollection({
+  type: "content",
+  schema: z.object({
+    date: z.string(),
+    title: z.string(),
+    color: z.string(),
+    description: z.string(),
+  }),
+});
+
+const grades = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    teacher: z.object({
+      name: z.string(),
+      subject: z.string(),
+      photo: z.string(),
+      age: z.number(),
+      birthday: z.string(), // Si quieres, se puede hacer más estricto con z.string().date()
+      address: z.string(),
+      favorites: z.object({
+        color: z.string(),
+        food: z.string(),
+        pet: z.string(),
+        music: z.string(),
+        movie: z.string(),
+        season: z.string(),
+        place: z.string(),
+        sports: z.string(),
+        subject: z.string(),
+      }),
+    }),
+  }),
+});
+
+export const collections = {
+  events,
+  grades, // 👈 este es el nombre de la colección
+};
---
+++ src/content/events/events.md
@@ -0,0 +1,11 @@
+---
+events:
+  - date: "2025-08-20"
+    title: "Día de Bienvenida"
+    color: "bg-indigo-500"
+    description: "Ceremonia de apertura del año escolar en el auditorio principal."
+  - date: "2025-08-28"
+    title: "Reunión de Padres"
+    color: "bg-green-500"
+    description: "Encuentro con padres para presentar el plan de estudios."
+---
